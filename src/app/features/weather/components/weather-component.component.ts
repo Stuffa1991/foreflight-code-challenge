a7e97f76466bd91bf27a4b0412d1ce64987e9c51
Staffan Enemark <sse@ordbogen.com>
2025-04-28 16:31:38 +0200

    Changes Add Eslint Add weather component

---
+++ src/app/features/weather/components/weather-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-component',
+  imports: [],
+  templateUrl: './weather-component.component.html',
+  styleUrl: './weather-component.component.scss',
+  standalone: true
+})
+export class WeatherComponentComponent {
+
+}
