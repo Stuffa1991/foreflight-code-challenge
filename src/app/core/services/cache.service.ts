c5c6fee46be65c9c537e740a56e6f1011eb0309d
Staffan Enemark <sse@ordbogen.com>
2025-04-30 16:03:44 +0200

    Changes Simplify cache

--- src/app/core/services/cache.service.ts
+++ src/app/core/services/cache.service.ts
@@ -8,17 +8,17 @@ export class CacheService {
   private readonly CACHE_PREFIX = 'weather_';
   private readonly CACHE_EXPIRY = 5 * 60 * 1000; // 5 minutes in milliseconds
 
-  getFromCache<T>(cacheKey: string): T | null {
+  getFromCache<T>(key: string): T | null {
     try {
-      const combinedCacheKey = this.CACHE_PREFIX + cacheKey;
-      const cachedItem = localStorage.getItem(combinedCacheKey);
+      const cacheKey = this.getCacheKey(key);
+      const cachedItem = localStorage.getItem(cacheKey);
 
       if (!cachedItem) return null;
 
       const { data, timestamp }: CacheModel<T> = JSON.parse(cachedItem);
 
       if (Date.now() - timestamp > this.CACHE_EXPIRY) {
-        localStorage.removeItem(combinedCacheKey);
+        localStorage.removeItem(cacheKey);
         return null;
       }
 
@@ -29,17 +29,20 @@ export class CacheService {
     }
   }
 
-  updateCache<T>(cacheKey: string, response: T): void {
+  updateCache<T>(key: string, response: T): void {
     try {
-      const combinedCacheKey = this.CACHE_PREFIX + cacheKey;
       const cacheData: CacheModel<T> = {
         data: response,
         timestamp: Date.now(),
       };
 
-      localStorage.setItem(combinedCacheKey, JSON.stringify(cacheData));
+      localStorage.setItem(this.getCacheKey(key), JSON.stringify(cacheData));
     } catch (error) {
       console.error('Error writing to localStorage:', error);
     }
+  }
+
+  private getCacheKey(cacheKey: string): string {
+    return this.CACHE_PREFIX + cacheKey;
   }
 }
